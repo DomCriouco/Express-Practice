12cc8f64b1bb9025d7a07db8da836f59bf382cbc
Enzo Soares <enzo.soares@galitt.com>
2022-03-11 11:30:20 +0100

    Adding Eslint to project

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ const app = express();
 const port = process.env.Port || 3000;
 
 app.get('/', (req:Request, res:Response) => {
-    res.send('It works!');
+  res.send('It works!');
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
