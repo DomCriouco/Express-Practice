f70b39a3b1001a40e41ce57d7bcb1ba940f295c3
Enzo Soares <enzo.soares@galitt.com>
2022-03-14 10:22:06 +0100

    Creating books route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import {Request, Response} from 'express';
 
 const express = require('express');
+const bookRouter = new express.Router();
 const app = express();
 const port = process.env.Port || 3000;
+
+bookRouter.route('/books')
+    .get((req:Request, res:Response) => {
+      const response = {hello: 'this is my book API'};
+      res.json(response);
+    });
+
+app.use('/api', bookRouter);
 
 app.get('/', (req:Request, res:Response) => {
   res.send('It works!');
