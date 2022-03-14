04c5cd915b646cc705e0d7a7a722642dcf746ac8
Enzo Soares <enzo.soares@galitt.com>
2022-03-14 15:18:51 +0100

    Adding post book to the api

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import {Request, Response} from 'express';
+import {connect} from 'mongoose';
+import {urlencoded, json} from 'body-parser';
+import Book from '../models/bookmodel';
 
 const express = require('express');
-const bookRouter = new express.Router();
 const app = express();
 const port = process.env.Port || 3000;
+// eslint-disable-next-line no-unused-vars
+const db = connect('mongodb://localhost/bookAPI');
+
+app.use(urlencoded({extended: true}));
+app.use(json());
+
+const bookRouter = new express.Router();
 
 bookRouter.route('/books')
     .get((req:Request, res:Response) => {
       const response = {hello: 'this is my book API'};
       res.json(response);
+    })
+    .post((req:Request, res:Response) => {
+      const book = new Book(req.body);
+
+      console.log(book);
+      return res.json(book);
     });
 
 app.use('/api', bookRouter);
