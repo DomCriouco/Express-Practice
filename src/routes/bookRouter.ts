d86c58cff33d48bf088d7e5cce071bf8a2623f40
Enzo Soares <enzo.soares@galitt.com>
2022-03-15 13:42:26 +0100

    Creating put request

--- src/routes/bookRouter.ts
+++ src/routes/bookRouter.ts
@@ -1,15 +1,16 @@
 import {bookInterface, bookSearch} from '../interfaces/bookinterface';
 import {Request, Response, Router} from 'express';
-import Bookmodel from '../../models/bookmodel';
+import {model} from 'mongoose';
+import BookModel from '../../models/bookmodel';
 
 const express = require('express');
 
-const routes = (BookModel: typeof Bookmodel) => {
+const routes = (bookModel: typeof BookModel) => {
   const bookRouter: Router = new express.Router();
 
   bookRouter.route('/books')
       .post((req:Request, res:Response) => {
-        const book = new Bookmodel(req.body);
+        const book = new BookModel(req.body);
 
         try {
           book.save();
@@ -31,7 +32,7 @@ const routes = (BookModel: typeof Bookmodel) => {
           myQuery.read = Boolean(req.query.read);
         }
 
-        BookModel.find(myQuery, (err, books) => {
+        bookModel.find(myQuery, (err, books) => {
           if (err) {
             return res.send(err);
           }
@@ -41,12 +42,30 @@ const routes = (BookModel: typeof Bookmodel) => {
 
   bookRouter.route('/books/:bookId')
       .get((req:Request, res:Response) => {
-        BookModel.findById(req.params.bookId,
+        bookModel.findById(req.params.bookId,
             (err: Error, book: bookInterface)=>{
               if (err) {
                 return res.send(err);
               }
               res.json(book);
+            });
+      })
+      .put((req:Request, res:Response) => {
+        bookModel.findById(req.params.bookId,
+            (err: Error, book: bookInterface)=>{
+              if (err) {
+                return res.send(err);
+              }
+
+              book.title = req.body.title;
+              book.author = req.body.author;
+              book.genre = req.body.genre;
+              book.read = req.body.read;
+
+              const modifiedBook = new BookModel(book);
+              modifiedBook.save();
+
+              return res.json(book);
             });
       });
 
